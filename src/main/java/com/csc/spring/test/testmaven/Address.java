5e6f86b2d38e4feee5495992ef1c5f9e0fb0bc42
Neeraj Kumar <contactdhawan@gmail.com>
2015-11-15 06:49:38 -0500

    making more errors

--- src/main/java/com/csc/spring/test/testmaven/Address.java
+++ src/main/java/com/csc/spring/test/testmaven/Address.java
@@ -10,18 +10,14 @@ return "Address [street=" + street + ", zipcode=" + zipcode + "]";
 public String getStreet() {
 return street;
 }
-public void setStreet(String street) {
-this.street = street;
+public void setStreet(String value) {
+this.street = value;
 }
 public String getZipcode() {
 return zipcode;
 }
-public void setZipcode(String zipcode) {
-	if(true){
-		
-	}
-	if(zipcode=="123"){zipcode="120";}
-this.zipcode = zipcode;
+public void setZipcode(String value) {
+this.zipcode = value;
 }
 
 public void init(){
